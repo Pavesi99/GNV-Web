47d8f9fa379d620b76237969204f53428352de55
Vilmar Pavesi Junior <juniorpavesi999@gmail.com>
2021-10-05 21:26:03 -0300

    Iniciado tela de CadastroLocal

---
+++ GNV Web/Controllers/CadastroLocalController.cs
@@ -0,0 +1,23 @@
+﻿using GNV_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GNV_Web.Controllers
+{
+    public class CadastroLocalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody] Local local )
+        {
+            return View();
+        }
+    }
+}
---
+++ GNV Web/Models/Local.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GNV_Web.Models
+{
+    public class Local
+    {
+        public Local(string nome, string endereco)
+        {
+            Nome = nome;
+            Endereco = endereco;
+        }
+
+        public string Nome { get; private set; }
+        public string Endereco { get; private set; }
+    }
+}
